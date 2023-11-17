26aba876bfb6e38fe45bbef5cc1740dcb5b552f5
iZeevens <129598659+iZeevens@users.noreply.github.com>
2023-11-17 22:44:08 +0600

    feat: addextractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -443,8 +443,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.split(' ').slice(1, 3).join(' ').slice(0, -1);
 }
 
 /**
