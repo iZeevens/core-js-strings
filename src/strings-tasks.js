2e6fd823753683b07c9386ad05c49a9d4a0b1860
iZeevens <129598659+iZeevens@users.noreply.github.com>
2023-11-17 20:58:57 +0600

    feat: add countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -325,8 +325,10 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return str && str.match(/[aeiouy]/gi) !== null
+    ? str.match(/[aeiouy]/gi).length
+    : 0;
 }
 
 /**
